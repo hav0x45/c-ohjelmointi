f9e160d77a0ce42c7c6c76a88e758cc411f406c3
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2025-01-27 16:11:13 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    int a = 10;
+    char string[] = "string";
+
+    printf("Console log: %d\n", a);
+    printf("Console log: ");
+    printf(string);
+
+    return 0;
+}
